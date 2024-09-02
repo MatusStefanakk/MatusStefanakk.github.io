d65600611d7b1574c14c78f7e08358e1272ff191
Matúš Štefaňák <>
2024-09-02 19:01:20 +0200

    .

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function getData(){
     var div = document.getElementById("dc_data");
 
-    
+    var row = document.createElement("tr");  //table row
+
+    var nameCell = document.createElement("td");  //table name data
+    nameCell.textContent = "random meno";
+    row.appendChild(nameCell);
+
+    var roleCell = document.createElement("td");  //table role data
+    roleCell.textContent = "random rola";
+    row.appendChild(roleCell);
+
+    div.appendChild(row);
 }
